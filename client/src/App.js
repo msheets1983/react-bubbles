63d4443d8abdbd8a8b6fe774ae757c880a886561
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-06-19 12:33:32 -0500

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
+
 function App() {
   return (
     <Router>
@@ -13,6 +16,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path="/bubble-page" component={BubblePage} />
       </div>
     </Router>
   );
